393e04e16bce88aaed2d37e7962e70ed6b08fd0d
Dyas <dyas@live.com>
2017-05-31 10:24:15 +0700

    Paginator instance methods

--- resources/views/fitur/link.blade.php
+++ resources/views/fitur/link.blade.php
@@ -16,5 +16,19 @@
         </tr>
         @endforeach
     </table>
-    {{ $links->links() }}
+    <!-- The Pagination -->
+    {{ $links->links() }} <br> 
+    <!-- ///////////////// -->
+
+    Element in current page : {{ $links->count() }} <br>
+    This is Page : {{ $links->currentPage() }} <br>
+    First Item is element number : {{ $links->firstItem() }} <br>
+    Last Item is element number : {{ $links->lastItem() }} <br>
+    (First Item and Last Item number from all amount, not ID) <br>
+    HasMorePage (boolean) : {{ $links->hasMorePages() }} <br>
+    LastPage : {{ $links->lastPage() }} <br>
+    NextPageURL : {{ $links->nextPageUrl() }} <br>
+    PreviousPageURL : {{ $links->previousPageUrl() }} <br>
+    How many element perpage (constant) : {{ $links->perPage() }} <br>
+    Total data retrieved (onstant) : {{ $links->total() }} <br>
 @endsection
